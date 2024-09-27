0b92ca5fcb3761b2ae21b2d53d980cc5d4389c76
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-09-27 23:23:10 +0300

    removing twilio

--- managment/views.py
+++ managment/views.py
@@ -18,26 +18,10 @@ class CreateSermon(View):
         return render(request, template_name='managment/create_sermon.html')
 
     def post(self, request):
-        from twilio.rest import Client
         book=request.POST['book']
         verse=request.POST['verse']
         description = request.POST['description']
         members = Member.objects.all()
-        for member in members:
-            print(member.phone_number)
-            try:
-                account_sid = 'AC565bc45825da07a5157ba5ef0f867928'
-                auth_token = '1def00172e44b6b0deb3e23217cebc30'
-                client = Client(account_sid, auth_token)
-
-                message = client.messages.create(
-                from_='whatsapp:+14155238886',
-                body=f'The message for today comes from the book of  {book} verse {verse}.\n--------Description------\n{description}',
-                to=f'whatsapp:+254{member.phone_number}'
-                )
-
-            except:
-                pass
         messages.success(request, message='The sermon has been sent successfully')
         return render(request,  template_name='managment/create_sermon.html')
 
