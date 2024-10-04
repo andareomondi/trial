8371a487423a8663cd6974edb98ac8cb0f98b622
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-10-04 20:12:23 +0300

    making changes on the dashboard

--- managment/views.py
+++ managment/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import redirect, render
 from django.views import View
-from member.models import Member, Sermon, Point
+from member.models import Member
 from django.contrib import messages
 # Create your views here.
 class Dashboard(View):
     def get(self, request):
         if request.user.is_superuser:
             members = Member.objects.all()
+            memberscount = members.count()
             context = {
-                'members': members
+                'members': members,
+                'memberscount': memberscount,
             }
             return render(request, 'managment/dashboard.html', context=context)
         else:
             return redirect(to='login')
-class CreateSermon(View):
-    def get(self, request):
-        return render(request, template_name='managment/create_sermon.html')
-
     def post(self, request):
-        book=request.POST['book']
-        verse=request.POST['verse']
-        description = request.POST['description']
+        member_id = request.POST.get('id')
+        member = Member.objects.get(id=member_id)
+        member.delete()
+        messages.success(request,  'Member deleted successfully')
         members = Member.objects.all()
-        messages.success(request, message='The sermon has been sent successfully')
-        return render(request,  template_name='managment/create_sermon.html')
-
+        memberscount = members.count()
+        context = {
+            'members': members,
+            'memberscount': memberscount,
+        }
+        return render(request,  'managment/dashboard.html', context=context)
