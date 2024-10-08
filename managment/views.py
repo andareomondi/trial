b9e01e79ced1f15c00ee1f44c258608af4c6ae3f
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-10-08 11:04:58 +0300

    delete function

--- managment/views.py
+++ managment/views.py
@@ -5,21 +5,23 @@ from django.contrib import messages
 # Create your views here.
 class Dashboard(View):
     def get(self, request):
-        if request.user.is_superuser:
-            members = Member.objects.all()
-            memberscount = members.count()
-            context = {
-                'members': members,
-                'memberscount': memberscount,
-            }
-            return render(request, 'managment/dashboard.html', context=context)
-        elif request.user.is_authenticated:
-            return redirect(to='home')
+        if request.user.is_authenticated:
+            if request.user.is_superuser:
+                members = Member.objects.all()
+                memberscount = members.count()
+                context = {
+                    'members': members,
+                    'memberscount': memberscount,
+                }
+                return render(request,  'managment/dashboard.html', context=context)
+            else:
+                return redirect('home')
         else:
-            return redirect(to='login')
+            return redirect('login')
     def post(self, request):
         member_id = request.POST.get('id')
         member = Member.objects.get(id=member_id)
+        # print(member_id)
         member.delete()
         messages.success(request,  'Member deleted successfully')
         members = Member.objects.all()
